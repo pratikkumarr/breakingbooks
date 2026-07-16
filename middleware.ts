40a6feb0ea7b6017008b10c18f8a0a5357768fb9
Pratik Kumar <pratiikkumarr@gmail.com>
2026-07-16 09:30:47 +0530

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,6 @@
+console.log('SUPABASE_URL exists:', !!process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log('SUPABASE_ANON_KEY exists:', !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
