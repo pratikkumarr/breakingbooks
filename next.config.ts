79fa08cfb036e8a991f86c90a99f9ab288cbef11
Pratik Kumar <pratiikkumarr@gmail.com>
2026-07-16 10:10:43 +0530

    config update

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { nextRuntime, webpack }) => {
+    if (nextRuntime === 'edge') {
+      config.plugins.push(
+        new webpack.DefinePlugin({ __dirname: '""' })
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
